324e81c223951634c5c0e105052a815d3d50ecf9
jiangtammy <53345362+jiangtammy@users.noreply.github.com>
2019-07-26 10:46:06 -0400

    Add files via upload

---
+++ dat1_nondiff_pred_sim_1000iterations_mda_06062019_par.R
@@ -0,0 +1,60 @@
+#QBA random forests - non-differential misclassification of predictor for simulated data set #1
+#March 21, 2019
+library(foreign)
+library(randomForest)
+library(pROC)
+
+#Get arguments from the command line
+argv <- commandArgs(TRUE)
+
+# Check if the command line is not empty and convert values to numerical values
+if (length(argv) > 0){
+  myse <- as.numeric( argv[1] )
+  mysp <- as.numeric( argv[2] ) 
+} else {
+  stop("Please input parameters")
+  #myse <- 0.95
+  #mysp <- 0.95
+}
+
+################################# True data set #1 #################################
+################################# Data set #1: Non-differential misclassification of predictor x5 #################################
+dat1.nondiff.misc.pred.func <- function(i, se, sp){
+  
+  n=10000
+  dat1 = data.frame(x1=rbinom(n, 1, 0.1),
+                    x2=rbinom(n, 1, 0.1),
+                    x3=rbinom(n, 1, 0.1),
+                    x4=rbinom(n, 1, 0.1),
+                    x5=rbinom(n, 1, 0.1),
+                    y=rbinom(n, 1, 0.2))
+  
+  dat1 <- data.frame(lapply(dat1, factor))
+  
+dat1.nondiff.pred <- dat1
+
+dat1.nondiff.pred$x5.new <- NA
+dat1.nondiff.pred$x5.new[dat1.nondiff.pred$x5==1] <- rbinom(length(which(dat1.nondiff.pred$x5==1)), 1, se)
+dat1.nondiff.pred$x5.new[dat1.nondiff.pred$x5==0] <- rbinom(length(which(dat1.nondiff.pred$x5==0)), 1, 1-sp)
+
+#Subset data for misclassified variables and the other variables of interest
+vars <- c("x1", "x2", "x3", "x4", "x5.new", "y")
+dat1.nondiff.pred2 <- dat1.nondiff.pred[vars] 
+
+dat1.nondiff.pred2$x5.new <- as.factor(dat1.nondiff.pred2$x5.new)
+
+#Run random forest for misclassified data set #1 with nondiff misclass. of predictor
+rf.dat1.nondiff.pred <- randomForest(y ~., data=dat1.nondiff.pred2, importance=T)
+
+#Variable importance
+importance(rf.dat1.nondiff.pred, type=1)
+}
+
+
+#Repeat the whole process
+dat1.nondiff.misc.pred.func.result <- t(sapply(1:1000, dat1.nondiff.misc.pred.func, se=myse,sp=mysp))
+
+write.csv(dat1.nondiff.misc.pred.func.result, col.names=F,
+          paste0("results/dat1_nondiff_misc_predictor_myse_", myse, "_mysp_", mysp, "_", 
+                 format(Sys.time(), "%m%d%Y_%s"), ".csv"),
+          quote=F, row.names=F)
